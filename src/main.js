b1937a3ec8fc8a434f005f391793852e629c8bfb
Aleksei Ogarkov <alexey.ogarkov@gmail.com>
2014-05-30 03:36:53 +0400

    Added function substitution

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@
  * @param obj future singleton object
  * @returns object with same fields that is singleton across tabs
  */
-var MultiTabSingleton = function(name,obj) {
+ var MultiTabSingleton = function(name,obj) {
 
   var store = window.$.jStorage || window.jStorage;
 
@@ -16,15 +16,51 @@ var MultiTabSingleton = function(name,obj) {
   }
 
   if (ObjectObserver === null) {
-    throw new ReferenceError("observer-js is not exists"); 
+    throw new ReferenceError("observer-js is not exists");
   }
 
-  var substituteFunctionsInObject = function(obj) {
+  var __toString = Object.prototype.toString;
 
-  }
-  
+  var substituteFunctionsInObject = function(obj, fn, iterPosParam) {
+
+    var ret, objType, iterPos = iterPosParam || [], i = 0;
+
+
+    if (__toString.call(obj) === '[object Array]') {
+      obj.forEach(function (item) {
+        objType = __toString.call(item);
+        if ( (objType === "[object Object]") || (objType === "[object Array]") ) {
+          ret = substituteFunctionsInObject(item, fn, iterPos.concat(i));
+        } else {
+          ret = fn(iterPos.concat(i), item, obj);
+        }
+
+        if (ret) {
+          obj[i] = ret;
+        }
+        i++;
+      });
+    }
+    for (var key in obj) {
+      if (!("" + key).match(/^\d+$/)) {
+        objType = __toString.call(obj[key]);
+        if ( (objType === "[object Object]") || (objType === "[object Array]") ) {
+          ret = substituteFunctionsInObject(obj[key], fn, iterPos.concat(key));
+        } else {
+          ret = fn(iterPos.concat(key), obj[key], obj);
+        }
+
+        if (ret) {
+          obj[key] = ret;
+        }
+      }
+    }
+
+    return obj;
+  };
+
   var observer = new ObjectObserver(obj);
-  
+
   observer.open(function(added, removed, changed, getOldValueFn) {
     // respond to changes to the obj.
     Object.keys(added).forEach(function(property) {
@@ -43,7 +79,13 @@ var MultiTabSingleton = function(name,obj) {
   });
 
   //also we need to mock all functions we will find in that object
+  //so we can execute function only on master singleton object
 
+  obj = substituteFunctionsInObject(obj, function(path,item,parent) {
+
+  });
+
+  obj.substituteFunctionsInObject = substituteFunctionsInObject;
 
   return obj;
 };
--- test/all.js
+++ test/all.js
@@ -22,3 +22,38 @@ test("the base function returns object with same properties and functions", func
   ok(typeof obj.testFunc === 'function')
   ok(typeof obj.testObj === 'object')
 });
+
+test("the function function substitution works", function() {
+  var obj = MultiTabSingleton('TestSubstitution', {
+    test1 : function() {
+      return "test1";
+    },
+    test2 : function(param) {
+      return "test2"+param;
+    },
+    testArr : [
+      function() {return "arr"}
+    ],
+    testObj : {
+      test4 : function() {
+        return "test4";
+      }
+    }
+  });
+
+  obj = obj.substituteFunctionsInObject(obj, function(path,item,parent) {    
+    if (typeof item === 'function') {      
+      return function() {
+        var args = Array.prototype.slice.call(arguments)
+        result = item.apply(parent, args);
+        return "wrap"+result;
+      }
+    }
+  });
+
+  ok(obj)
+  ok(obj.test1() === 'wraptest1')
+  ok(obj.test2('param') === 'wraptest2param')
+  ok(obj.testArr[0]() === 'wraparr')
+  ok(obj.testObj.test4() === 'wraptest4')
+});
